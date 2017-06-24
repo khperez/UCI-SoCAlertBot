02633cd419eee1d308ce3b4141e067a151317db1
Katherine Perez <khperez@uh.edu>
2017-06-24 13:09:50 -0700

    Retrieve text-based EECS 221 results

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import scrapy
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
@@ -5,3 +6,8 @@ driver = webdriver.Firefox()
 driver.get('http://www.reg.uci.edu/perl/WebSoc')
 department_select = Select(driver.find_element_by_name('Dept'))
 department_select.select_by_value("EECS")
+coursenum_element = driver.find_element_by_xpath("//input[@name='CourseNum']")
+coursenum_element.send_keys("221")
+submit_element = driver.find_element_by_xpath("//input[@value='Display Text Results']")
+submit_element.click()
+
